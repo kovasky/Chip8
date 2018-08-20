1551b3b957f574119db72e4a516122665c6d4f3b
kovasky <kab310@mun.ca>
2018-08-20 17:14:50 -0230

    keypad input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
+#include <map>
 #include <string>
 #include <SDL2/SDL.h> 
 #include "Chip8.h"
 #include "Display.h"
+#include <iostream>
+
+using Register8 = uint8_t;
+
+std::pair<Register8, Chip8::keyTrigger> getKey();
+
+std::map<SDL_Keycode,Register8> myKeys = 
+{
+  {SDLK_1, 0x1},
+  {SDLK_2, 0x2},
+  {SDLK_3, 0x3},
+  {SDLK_4, 0xC},
+  {SDLK_q, 0x4},
+  {SDLK_w, 0x5},
+  {SDLK_e, 0x6},
+  {SDLK_r, 0xD},
+  {SDLK_a, 0x7},
+  {SDLK_s, 0x8},
+  {SDLK_d, 0x9},
+  {SDLK_f, 0xE},
+  {SDLK_z, 0xA},
+  {SDLK_x, 0x0},
+  {SDLK_c, 0xB},
+  {SDLK_v, 0xF},
+};
 
 int main(int argv, char *args[])
 {
@@ -15,10 +41,11 @@ int main(int argv, char *args[])
 	{
 		myChip->run();
 
+		myChip->updateKeys(getKey());
+
 		if(myChip->updateDisplay())
 		{
 			myDisplay->drawImage(myChip->displayMem());
-			myChip->updateDisplay(false);
 		}
 	}
 
@@ -27,4 +54,28 @@ int main(int argv, char *args[])
 	delete myDisplay;
 
 	return 0;
+}
+
+std::pair<Register8, Chip8::keyTrigger> getKey()
+{
+	SDL_Event someEvent;	
+	
+	Register8 mappedValue = 0x00;
+
+	Chip8::keyTrigger someTrigger = Chip8::keyTrigger::keyRelease;
+
+	while(SDL_PollEvent(&someEvent))
+	{
+		auto someKey = someEvent.key.keysym.sym;
+		
+		if(myKeys.find(someKey) != myKeys.end())
+		{
+			mappedValue = myKeys[someKey];
+
+			someTrigger = someEvent.type == SDL_KEYDOWN? Chip8::keyTrigger::keyPress : Chip8::keyTrigger::keyRelease;
+		}
+
+	}
+
+	return {mappedValue,someTrigger};
 }
